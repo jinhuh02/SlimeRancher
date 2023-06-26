7119378831669ccc77b66badc32b3d545718ce9b
Jin <jinnus.heo@gmail.com>
2023-06-26 17:26:27 +0900

    [file] 폴더정리

--- SlimeRancher/Assets/3.Script/PlayerController.cs
+++ SlimeRancher/Assets/3.Script/PlayerController.cs
@@ -29,19 +29,19 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            gameObject.transform.position += Vector3.forward * speed * Time.deltaTime;
+            rigidbody.velocity = Vector3.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position += Vector3.back * speed * Time.deltaTime;
+            rigidbody.velocity = Vector3.back * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += Vector3.left * speed * Time.deltaTime;
+            rigidbody.velocity = Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+            rigidbody.velocity = Vector3.right * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.Space))
@@ -49,29 +49,6 @@ public class PlayerController : MonoBehaviour
             rigidbody.AddForce(Vector3.up * 40, ForceMode.Force);
         }
 
-
-        //mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //GroupPlane = new Plane(Vector3.up, Vector3.zero);
-
-        //float rayLength;
-
-        //if (GroupPlane.Raycast(mousePosition, out rayLength))
-        //{
-        //    Vector3 rotate_Value = mousePosition.GetPoint(rayLength);
-        //    Debug.Log(rotate_Value);
-
-        //    //transform.LookAt -> 위에서 구한 pointTolook 위치값을 캐릭터가 바라보도록 한다
-        //    //transform.LookAt(new Vector3(rotate_Value.x, transform.position.y, rotate_Value.z));
-        //    if (rotate_Value.x < -300)
-        //    {
-        //        transform.Rotate(0, -1, 0, Space.Self);
-        //    }
-        //    else if (rotate_Value.x > 120)
-        //    {
-        //        transform.Rotate(0, 1, 0, Space.Self);
-        //    }
-        //}
 
         mousePos = Input.mousePosition;
         worldPos = Camera.main.ScreenToViewportPoint(mousePos);
