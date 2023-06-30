aa692393d526803256f2dfa922f3028fdec2d02b
Jin <jinnus.heo@gmail.com>
2023-06-30 17:54:42 +0900

    [fix] 인벤토리에서 아이템 내보낼 때의 위치 수정

--- SlimeRancher/Assets/3.Script/Facility/Market.cs
+++ SlimeRancher/Assets/3.Script/Facility/Market.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Market : MonoBehaviour
 {
+    [SerializeField] Text coin_text;
+    int coin = 0;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Item"))
         {
             Debug.Log("1개 접수! 코인증가!");
             Destroy(collision.gameObject);
+            coin += 100;
+            coin_text.text = ""+coin;
         }
     }
 }
