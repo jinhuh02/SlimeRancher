2a67219d3b8b75128794f2c871c21e22c16d4139
Jin <jinnus.heo@gmail.com>
2023-07-06 17:13:04 +0900

    [save]

---
+++ SlimeRancher/Assets/3.Script/ETC/AudioManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    #region instance
+    public static AudioManager instance = null;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    [SerializeField] AudioSource background_Audio; //music
+    [SerializeField] AudioSource effect_Audio; //마켓소리, ui소리 등
+    //슬라임은 각자 오디오 소스 갖고있음 Slim, SlimeSpawner
+    //진공팩도 갖고있음 Inventory
+    //플레이어도 갖고있음 PlayerController
+
+    [SerializeField] List<AudioClip> backgroundClip = new List<AudioClip>();
+    [SerializeField] List<AudioClip> effectClip = new List<AudioClip>();
+
+    public void PlayBackgroundAudio(int clipNum)
+    {
+        background_Audio.clip = backgroundClip[clipNum];
+        background_Audio.Play();
+    }
+
+    public void PlayEffectAudio(int clipNum)
+    {
+        effect_Audio.clip = effectClip[clipNum];
+        effect_Audio.Play();
+    }
+
+    public void StopAudio()
+    {
+        if (background_Audio.isPlaying)
+        {
+            background_Audio.Stop();
+        }
+
+        if (effect_Audio.isPlaying)
+        {
+            effect_Audio.Stop();
+        }
+    }
+}
